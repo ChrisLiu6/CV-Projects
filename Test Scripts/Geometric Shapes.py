7be53172425d4c5e1e0c3cd8da684b4e278ed55e
Chris Liu <liu.6948@osu.edu>
2019-09-22 00:13:23 -0400

    New line

--- Test Scripts/Geometric Shapes.py
+++ Test Scripts/Geometric Shapes.py
@@ -3,16 +3,13 @@ import numpy as np
 
 img = cv2.imread('chris.jpg', 1)
 
-img = cv2.line(img, (50.50), (100,100), (255,0,0), 3)
+img = cv2.line(img, (50, 50), (2000, 2000), (100, 100, 0), 10)
 
-cv2.imshow(img)
+cv2.imshow('image', img)
 
-if cv2.waitKey(0) == ord('s'):
-    cv2.imwrite('cool dude but with line', img)
+key = cv2.waitKey(0)
+if key == ord('s'):
+    cv2.imwrite('cool dude but with line.jpg', img)
     cv2.destroyAllWindows()
 else:
     cv2.destroyAllWindows()
-
-
-
-
