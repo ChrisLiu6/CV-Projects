533fd121453fc1a5c1170a6babb6a884d2b0ab6c
Chris Liu <liu.6948@osu.edu>
2019-09-22 00:29:21 -0400

    Updated box and text

--- Test Scripts/Geometric Shapes.py
+++ Test Scripts/Geometric Shapes.py
@@ -3,7 +3,9 @@ import numpy as np
 
 img = cv2.imread('chris.jpg', 1)
 
-img = cv2.line(img, (50, 50), (2000, 2000), (100, 100, 0), 10)
+img = cv2.rectangle(img, (1100, 500), (2300, 1800), (0, 0, 255), 20)
+font = cv2.FONT_HERSHEY_PLAIN
+img = cv2.putText(img, 'target', (1100, 400), font, 8, (0, 0, 255), 3, cv2.LINE_AA)
 
 cv2.imshow('image', img)
 
