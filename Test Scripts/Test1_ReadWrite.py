9ac87903a45fa2cf08cf74492a3793317dea7003
Chris Liu <liu.6948@osu.edu>
2019-09-21 21:59:13 -0400

    Update Test1_ReadWrite.py

--- Test Scripts/Test1_ReadWrite.py
+++ Test Scripts/Test1_ReadWrite.py
@@ -1,15 +1,31 @@
 import cv2
 
-image = cv2.imread('chris.jpg', 0) # Gray Scale
+#### Read and Write Photos
+##image = cv2.imread('chris.jpg', 0) # Gray Scale
+##
+##print(image)
+##
+##cv2.imshow('cool dude', image)
+##key = cv2.waitKey()
+##
+##if key == 27:
+##    cv2.destroyAllWindows()
+##elif key == ord('s'):
+##    cv2.imwrite('cool dude but gray.png', image)
+##    cv2.destroyAllWindows()
 
-print(image)
 
-cv2.imshow('cool dude', image)
-key = cv2.waitKey()
+### Video Capture
+cap = cv2.VideoCapture(0)
 
-if key == 27:
-    cv2.destroyAllWindows()
-elif key == ord('s'):
-    cv2.imwrite('cool dude but gray.png', image)
-    cv2.destroyAllWindows()
+while (cap.isOpened()):
+    ret, frame = cap.read()
 
+    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('video', frame_gray)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
